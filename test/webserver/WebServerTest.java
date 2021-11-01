b1c795a06d122d4b5e070fc9c426ace08eff3528
IoanBasnic <57294224+IoanBasnic@users.noreply.github.com>
2021-11-01 18:53:39 +0200

    Added Maven | Added JUnit 4 tests + Mokito | Fixed some bugs (#5)

--- test/webserver/WebServerTest.java
+++ test/webserver/WebServerTest.java
@@ -1,36 +1,61 @@
 package webserver;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 
 public class WebServerTest {
 
-    @Test
-    public void main() {
+    WebServer webServer;
+    WebServer webServerMock = mock(WebServer.class);
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
     }
 
     @Test
-    public void run() {
+    public void main() throws IOException {
+        String[] args = null;
+        webServer.main(args);
+        try {
+            ServerSocket serverSocket = new ServerSocket(10008);
+            webServer.main(args);
+        } catch (Exception e) {
+            System.out.println("Got the exception I wanted");
+        }
     }
 
     @Test
-    public void getPath() {
+    public void run() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(10016);
+        Socket clientSocket = serverSocket.accept();
+        try {
+            webServer.run();
+            fail();
+        } catch (Exception e) {
+            System.out.println("Got the exception");
+        }
     }
 
     @Test
-    public void fileFoundHeader() {
+    public void WebServer() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(10013);
+        Socket clientSocket = serverSocket.accept();
+        webServer = new WebServer(clientSocket);
     }
 
-    @Test
-    public void sendReply() {
-    }
-
-    @Test
-    public void errorHeader() {
-    }
-
-    @Test
-    public void openFile() {
-    }
 }
